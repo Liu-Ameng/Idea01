ec20afed724093f42deda67bca2143896871ed62
liu-ameng <liuche911@gmail.com>
2015-03-08 19:36:48 +0800

    nothing
    I do not know what will happen.

---
+++ webapp/js/router.js
@@ -0,0 +1,33 @@
+Todos.Router.map(function() {
+    this.resource('todos', {
+        path : '/'
+    }, function() {
+        // additional child routes will go here later
+        this.route('active');
+    });
+});
+
+Todos.TodosRoute = Ember.Route.extend({
+    model : function() {
+        return this.store.find('todo');
+    }
+});
+
+Todos.TodosIndexRoute = Ember.Route.extend({
+    model : function() {
+        return this.modelFor('todos');
+    }
+});
+
+Todos.TodosActiveRoute = Ember.Route.extend({
+    model : function() {
+        return this.store.filter('todo', function(todo) {
+            return !todo.get('isCompleted');
+        });
+    },
+    renderTemplate : function(controller) {
+        this.render('todos/index', {
+            controller : controller
+        });
+    }
+});
